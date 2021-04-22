9b665188fb5b79cdd2ed57eb4925167511d1e80b
Weaverzhu <weaver_zhu@outlook.com>
2021-04-22 08:59:51 +0800

    <> to [] for links, add time_link

--- functions/chain.defaults.fish
+++ functions/chain.defaults.fish
@@ -1,6 +1,12 @@
+function time_link
+  echo yellow
+  date '+%H:%M:%S'
+end
+
 function chain.defaults -d 'Reset prompt links to defaults'
   set -e chain_links
 
+  chain.push time_link
   chain.push chain.links.root
   chain.push chain.links.jobs
   chain.push chain.links.pwd
